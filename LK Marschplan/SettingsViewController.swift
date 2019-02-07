f74752fc0a1ef02063273eeb6d64f57a63d28048
Sam Ellenrieder <sam.ell@web.de>
2019-02-07 12:49:04 +0100

    first project push

---
+++ LK Marschplan/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  LK Marschplan
+//
+//  Created by Sam Ellenrieder on 23/01/2019.
+//  Copyright © 2019 Sam. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
