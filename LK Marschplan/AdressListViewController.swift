14bf6cff324825462c64428c1df9ad7dd17b6427
Sam Ellenrieder <sam.ell@web.de>
2019-02-10 18:36:14 +0100

    google universal link in function implemented

--- LK Marschplan/AdressListViewController.swift
+++ LK Marschplan/AdressListViewController.swift
@@ -24,11 +24,11 @@ class AdressListViewController: UIViewController {
     @IBOutlet var addressTable: UITableView!
     @IBOutlet var addressButton: UIButton!
     
-    var addresses = [
-        Address(id: 1, date: parseDate("22.07.2019"), time: "05:30", name: "Hugo Breitling", address: "FasanenWeg 7, 88214 Ravensburg", navigationButton: <#T##UIButton#>),
-        Address(id: 2, date: parseDate("23.07.2019"), time: "06:45", name: "Peter Maffay", address: "Am Dom 23, 88212 Ravensburg", navigationButton: <#T##UIButton#>),
-        Address(id: 3, date: parseDate("24.07.2019"), time: "06:15", name: "Arthur Binder", address: "Petersbrücke 42, 88213 Ravensburg", navigationButton: <#T##UIButton#>)
-    ]
+//    var addresses = [
+//        Address(id: 1, date: parseDate("22.07.2019"), time: "05:30", name: "Hugo Breitling", address: "Schützenstraße+7+Ravensburg", navigationButton: addressButton),
+//        Address(id: 2, date: parseDate("23.07.2019"), time: "06:45", name: "Peter Maffay", address: "Federburg+Straße+23+Ravensburg", navigationButton: addressButton),
+//        Address(id: 3, date: parseDate("24.07.2019"), time: "06:15", name: "Arthur Binder", address: "Marienplatz+3+Ravensburg", navigationButton: addressButton)
+//    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +36,7 @@ class AdressListViewController: UIViewController {
     }
     
     func openAddressInGoogle(_ address: String) {
-        let urlString = "https://www.google.com/maps/"
+        let urlString = "comgooglemaps://?saddr=&daddr=\(address)&directionsmode=walking"
         let url = URL(string: urlString)
         
         UIApplication.shared.openURL(url!)
@@ -47,6 +47,10 @@ class AdressListViewController: UIViewController {
         openAddressInGoogle("Hello")
         
     }
-
+    
+    @IBAction func testButton(_ sender: Any) {
+        openAddressInGoogle("hello")
+    }
+    
 }
 
