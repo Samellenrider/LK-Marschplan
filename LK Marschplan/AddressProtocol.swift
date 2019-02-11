908ff7ff4833c0958d4c6f082ee810f5bc990c95
Sam Ellenrieder <sam.ell@web.de>
2019-02-11 14:56:25 +0100

    AddressList class implemented

--- LK Marschplan/AddressProtocol.swift
+++ LK Marschplan/AddressProtocol.swift
@@ -1,1 +1,32 @@
+import UIKit
 import Foundation
+
+class AddressList {
+
+  struct Address {
+    
+      var id: Int
+      var date: Date
+      var time: String
+      var name: String
+      var address: String
+      var navigationButton: UIButton
+    
+  }
+
+  var addressButton: UIButton!
+
+  func parseDate(_ str: String) -> Date {
+    
+      let dateformatter = DateFormatter()
+      dateformatter.dateFormat = "dd-MM-yyyy"
+      return dateformatter.date(from: str)!
+  }
+
+  var addresses = [
+      Address(id: 1, date: parseDate("22.07.2019"), time: "05:30", name: "Hugo Breitling", address: "Schützenstraße+7+Ravensburg", navigationButton: addressButton),
+      Address(id: 2, date: parseDate("23.07.2019"), time: "06:45", name: "Peter Maffay", address: "Federburg+Straße+23+Ravensburg", navigationButton: addressButton),
+      Address(id: 3, date: parseDate("24.07.2019"), time: "06:15", name: "Arthur Binder", address: "Marienplatz+3+Ravensburg", navigationButton: addressButton)
+  ]
+
+}
--- LK Marschplan/AdressListViewController.swift
+++ LK Marschplan/AdressListViewController.swift
@@ -1,34 +1,9 @@
 import UIKit
 import Foundation
 
-struct Address {
-    
-    var id: Int
-    var date: Date
-    var time: String
-    var name: String
-    var address: String
-    var navigationButton: UIButton
-    
-}
-
-fileprivate func parseDate(_ str: String) -> Date {
-    
-    let dateformatter = DateFormatter()
-    dateformatter.dateFormat = "dd-MM-yyyy"
-    return dateformatter.date(from: str)!
-}
-
 class AdressListViewController: UIViewController {
     
     @IBOutlet var addressTable: UITableView!
-    @IBOutlet var addressButton: UIButton!
-    
-//    var addresses = [
-//        Address(id: 1, date: parseDate("22.07.2019"), time: "05:30", name: "Hugo Breitling", address: "Schützenstraße+7+Ravensburg", navigationButton: addressButton),
-//        Address(id: 2, date: parseDate("23.07.2019"), time: "06:45", name: "Peter Maffay", address: "Federburg+Straße+23+Ravensburg", navigationButton: addressButton),
-//        Address(id: 3, date: parseDate("24.07.2019"), time: "06:15", name: "Arthur Binder", address: "Marienplatz+3+Ravensburg", navigationButton: addressButton)
-//    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
