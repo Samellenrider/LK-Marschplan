e6f31ef36318fb6c2ed2b723daaa9f6f85c14ed2
Sam Ellenrieder <sam.ell@web.de>
2019-02-14 16:17:14 +0100

    file changes to make development easier

--- LK Marschplan/AdressListViewController.swift
+++ LK Marschplan/AdressListViewController.swift
@@ -1,15 +1,46 @@
 import UIKit
 import Foundation
 
-let addressListClass = AddressListClass()
+//let addressListClass = AddressListClass()
+
+var addressButton: UIButton!
+
+struct Address {
+    
+    var id: Int
+    var date: Date
+    var time: String
+    var name: String
+    var address: String
+    var navigationButton: UIButton
+    
+}
+
+fileprivate func parseDate(_ str: String) -> Date {
+    
+    let dateformatter = DateFormatter()
+    dateformatter.dateFormat = "dd-MM-yyyy"
+    return dateformatter.date(from: str)!
+}
+
 
 class AdressListViewController: UIViewController {
     
+    var firstAdress: String?
+    
     @IBOutlet var addressTable: UITableView!
+    
+    var addresses = [
+        Address(id: 1, date: parseDate("22.07.2019"), time: "05:30", name: "Hugo Breitling", address: "Schützenstraße+7+Ravensburg", navigationButton: addressButton),
+        Address(id: 2, date: parseDate("23.07.2019"), time: "06:45", name: "Peter Maffay", address: "Federburg+Straße+23+Ravensburg", navigationButton: addressButton),
+        Address(id: 3, date: parseDate("24.07.2019"), time: "06:15", name: "Arthur Binder", address: "Marienplatz+3+Ravensburg", navigationButton: addressButton)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//        debugPrint(addresses[0].address)
+        
     }
     
     func openAddressInGoogle(_ address: String) {
@@ -19,18 +50,10 @@ class AdressListViewController: UIViewController {
         UIApplication.shared.open(url!, options: [:], completionHandler: nil)
     }
     
-    @IBAction func buttonPressed(_ sender: Any) {
-        
-        openAddressInGoogle("Hello")
-        
-    }
-    
-    
-    
-    
-    
-    
-    
+//    @IBAction func buttonPressed(_ sender: Any) {
+//
+//        openAddressInGoogle("Marienplatz+3+Ravensburg")
+//    }
     
     
     
@@ -39,7 +62,10 @@ class AdressListViewController: UIViewController {
     
     
     @IBAction func testButton(_ sender: Any) {
-        openAddressInGoogle("hello")
+        
+        firstAdress =  addresses[0].address
+        
+        openAddressInGoogle(firstAdress ?? "Tokyo")
     }
     
 }
